cff8c5baf2e94901c232070d42fab700c62154bc
sumecomorins <sumesh@comorins.com>
2022-05-02 16:07:16 +0530

    stack and tab navigation
    navigation setup finished

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function ProfileScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your jobs candidate app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function RegisterScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Register!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Search"
+        onPress={() => navigation.navigate('Search')}
+      />
+      <Button
+        title="Go to Register"
+        onPress={() => navigation.navigate('Register')}
+      />
+    </View>
+  );
+}
+
+function SearchScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Search screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SearchStack = createNativeStackNavigator();
+
+function SearchStackScreen() {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Search" component={SearchScreen} />
+      <SearchStack.Screen name="Profile" component={ProfileScreen} />
+    </SearchStack.Navigator>
+  );
+}
+
+const RegisterStack = createNativeStackNavigator();
+
+function RegisterStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Register" component={RegisterScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeStackScreen}  />
+        <Tab.Screen name="Search" component={SearchStackScreen} />
+        <Tab.Screen name="Register" component={RegisterStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
