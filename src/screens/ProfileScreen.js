edb468adf302c0bfa42a6489f1c41d547d3118ee
sumecomorins <sumesh@comorins.com>
2022-05-02 20:48:25 +0530

    search result ui
    search result ui and navigation to profile
    get route item in profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function ProfileScreen({ navigation }) {
+export default function ProfileScreen({ route, navigation }) {
+    console.log('route', route.params.item)
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile!</Text>
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,152 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
 export default function SearchScreen({ navigation }) {
+
+    const [data, setData] = useState([
+        {id:1, name: "Mark Doe",   position:"CEO",},               
+        {id:1, name: "John Doe",   position:"CTO",},              
+        {id:2, name: "Clark Man",  position:"Creative designer", },
+        {id:3, name: "Jaden Boor", position:"Front-end dev",  } , 
+        {id:4, name: "Srick Tree", position:"Backend-end dev",} , 
+        {id:5, name: "John Doe",   position:"Creative designer",},
+        {id:6, name: "John Doe",   position:"Manager",},          
+        {id:8, name: "John Doe",   position:"IOS dev", },        
+        {id:9, name: "John Doe",   position:"Web dev", },      
+        {id:9, name: "John Doe",   position:"Analyst", }    
+    ])
+
+    const clickEventListener = (item) => {
+        navigation.navigate('Profile', {
+            item: item,
+          })
+    }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text>Search screen</Text>
+    //   <Button
+    //     title="Go to Profile"
+    //     onPress={() => navigation.navigate('Profile')}
+    //   />
+    // </View>
+    <View style={styles.container}>
+        <FlatList style={styles.list}
+          contentContainerStyle={styles.listContainer}
+          data={data}
+          horizontal={false}
+          numColumns={2}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={styles.card} onPress={() => {clickEventListener(item)}}>
+                <View style={styles.cardHeader}>
+                  <Image style={styles.icon} source={{uri:"https://img.icons8.com/flat_round/64/000000/hearts.png"}}/>
+                </View>
+                {/* <Image style={styles.userImage} source={{uri:item.image}}/>  */}
+                <View style={styles.cardFooter}>
+                  <View style={{alignItems:"center", justifyContent:"center"}}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.position}>{item.position}</Text>
+                    <TouchableOpacity style={styles.followButton} onPress={()=>clickEventListener(item)}>
+                      <Text style={styles.followButtonText}>View </Text>  
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )
+          }}/>
+      </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      marginTop:20,
+    },
+    list: {
+      paddingHorizontal: 5,
+      backgroundColor:"#E6E6E6",
+    },
+    listContainer:{
+     alignItems:'center'
+    },
+    /******** card **************/
+    card:{
+      shadowColor: '#00000021',
+      shadowOffset: {
+        width: 0,
+        height: 6,
+      },
+      shadowOpacity: 0.37,
+      shadowRadius: 7.49,
+      elevation: 12,
+  
+      marginVertical: 5,
+      backgroundColor:"white",
+      flexBasis: '46%',
+      marginHorizontal: 5,
+    },
+    cardFooter: {
+      paddingVertical: 17,
+      paddingHorizontal: 16,
+      borderTopLeftRadius: 1,
+      borderTopRightRadius: 1,
+      flexDirection: 'row',
+      alignItems:"center", 
+      justifyContent:"center"
+    },
+    cardContent: {
+      paddingVertical: 12.5,
+      paddingHorizontal: 16,
+    },
+    cardHeader:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingTop: 12.5,
+      paddingHorizontal: 16,
+      borderBottomLeftRadius: 1,
+      borderBottomRightRadius: 1,
+    },
+    userImage:{
+      height: 120,
+      width: 120,
+      borderRadius:60,
+      alignSelf:'center',
+      borderColor:"#DCDCDC",
+      borderWidth:3,
+    },
+    name:{
+      fontSize:18,
+      flex:1,
+      alignSelf:'center',
+      color:"#008080",
+      fontWeight:'bold'
+    },
+    position:{
+      fontSize:14,
+      flex:1,
+      alignSelf:'center',
+      color:"#696969"
+    },
+    followButton: {
+      marginTop:10,
+      height:35,
+      width:100,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius:30,
+      backgroundColor: "#00BFFF",
+    },
+    followButtonText:{
+      color: "#FFFFFF",
+      fontSize:20,
+    },
+    icon:{
+      height: 20,
+      width: 20, 
+    }
+  });    
